c354f83b26ad7889bdc500b6fa1c3e68e0ea6a10
marcelosantana <marcelosantana@alu.ufc.br>
2023-01-10 18:20:07 -0300

    [NLW-eSports] - Finish Backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import cors from "cors";
 
 import { PrismaClient } from "@prisma/client";
+import { convertHourStringToMinutes } from "./utils/convert-hours-string-to-minutes";
+import { convertMinutesToHourString } from "./utils/convert-minutes-string-to-hours";
 
 const app = express();
 const prisma = new PrismaClient();
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/games", async (request, response) => {
   const games = await prisma.game.findMany({
@@ -18,8 +24,24 @@ app.get("/games", async (request, response) => {
   return response.json(games);
 });
 
-app.post("/ads", (request, response) => {
-  return response.status(201).json([]);
+app.post("/games/:id/ads", async (request, response) => {
+  const gameId = request.params.id;
+  const body = request.body;
+
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(","),
+      hourStart: convertHourStringToMinutes(body.hourStart),
+      hourEnd: convertHourStringToMinutes(body.hourEnd),
+      useVoiceChannel: body.useVoiceChannel,
+    },
+  });
+
+  return response.status(201).json(ad);
 });
 
 app.get("/games/:id/ads", async (request, response) => {
@@ -48,6 +70,8 @@ app.get("/games/:id/ads", async (request, response) => {
       return {
         ...ad,
         weekDays: ad.weekDays.split(","),
+        hourStart: convertMinutesToHourString(ad.hourStart),
+        hourEnd: convertMinutesToHourString(ad.hourEnd),
       };
     })
   );
