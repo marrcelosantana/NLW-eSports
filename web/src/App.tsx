94815c82ba55bc9eaed7daff412b91cb8b546422
marcelosantana <marcelosantana@alu.ufc.br>
2023-01-11 16:40:12 -0300

    [NLW-eSports] - Connect backend + frontend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,8 +4,20 @@ import CreateAdBanner from "./components/CreateAdBanner";
 import logoImg from "./assets/logo-nlw-esports.svg";
 
 import "./styles/main.css";
+import { useEffect, useState } from "react";
+import { Game } from "./models/Game";
 
 function App() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3333/games")
+      .then((response) => response.json())
+      .then((data) => {
+        setGames(data);
+      });
+  }, []);
+
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="logo" />
@@ -13,7 +25,13 @@ function App() {
         Seu duo está aqui.
       </h1>
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner bannerUrl="/game-2.png" title="Dota 2" adsCount={3} />
+        {games.map((game) => (
+          <GameBanner
+            bannerUrl={game.bannerUrl}
+            title={game.title}
+            adsCount={game._count.ads}
+          />
+        ))}
       </div>
       <CreateAdBanner />
     </div>
