ff5165477a95aa4d3df5951b4a523eba0cd4955d
marcelosantana <marcelosantana@alu.ufc.br>
2023-01-13 17:08:04 -0300

    [NLW-eSports] - Form

--- web/src/components/CreateAdModal.tsx
+++ web/src/components/CreateAdModal.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, FormEvent } from "react";
+import axios from "axios";
 
 import * as Dialog from "@radix-ui/react-dialog";
 import * as Checkbox from "@radix-ui/react-checkbox";
@@ -11,34 +12,63 @@ import { Check, GameController } from "phosphor-react";
 export function CreateAdModal() {
   const [games, setGames] = useState<Game[]>([]);
   const [weekDays, setWeekDays] = useState<string[]>([]);
+  const [useVoiceChannel, setUseVoiceChannel] = useState(false);
 
   useEffect(() => {
-    fetch("http://localhost:3333/games")
-      .then((response) => response.json())
-      .then((data) => {
-        setGames(data);
-      });
+    axios("http://localhost:3333/games").then((response) => {
+      setGames(response.data);
+    });
   }, []);
+
+  async function handleCreateAd(event: FormEvent) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target as HTMLFormElement);
+    const data = Object.fromEntries(formData);
+
+    if (!data.name) {
+      return;
+    }
+
+    try {
+      await axios.post(`http://localhost:3333/games/${data.game}/ads`, {
+        name: data.name,
+        yearsPlaying: Number(data.yearsPlaying),
+        discord: data.discord,
+        weekDays: weekDays.map(Number),
+        hourStart: data.hourStart,
+        hourEnd: data.hourEnd,
+        useVoiceChannel: useVoiceChannel,
+      });
+      alert("Anúncio criado com sucesso!!");
+    } catch (error) {
+      alert("Erro ao criar anúncio");
+    }
+  }
 
   return (
     <Dialog.Portal>
       <Dialog.Overlay className="bg-black/80 inset-0 fixed" />
       <Dialog.Content
         className="fixed bg-[#2A2634] py-8 px-10 text-white 
-        top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] 
+        top-1/2 left-1/2 -translate-x-1/2 ~
+        -translate-y-1/2 rounded-lg w-[480px] 
         shadow-lg shadow-black/25"
       >
         <Dialog.Title className="text-3xl font-black">
           Publique um anúncio
         </Dialog.Title>
-        <form className="mt-8 flex flex-col gap-4">
+        <form className="mt-8 flex flex-col gap-4" onSubmit={handleCreateAd}>
           <div className="flex flex-col gap-2">
             <label htmlFor="game" className="font-semibold">
               Qual o game?
             </label>
             <select
+              name="game"
               defaultValue=""
-              className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 appearance-none outline-none"
+              className="bg-zinc-900 py-3 px-4 rounded text-sm 
+              placeholder:text-zinc-500 appearance-none outline-none"
+              required
             >
               <option value="" disabled>
                 Selecione o game que deseja jogar
@@ -55,7 +85,12 @@ export function CreateAdModal() {
 
           <div className="flex flex-col gap-2">
             <label htmlFor="game">Seu nome (ou nickname)</label>
-            <Input placeholder="Como te chamam dentro do game?" />
+            <Input
+              placeholder="Como te chamam dentro do game?"
+              name="name"
+              id="name"
+              required
+            />
           </div>
 
           <div className="grid grid-cols-2 gap-6">
@@ -63,13 +98,21 @@ export function CreateAdModal() {
               <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
               <Input
                 type="number"
+                name="yearsPlaying"
                 id="yearsPlaying"
                 placeholder="Tudo bem ser ZERO"
+                required
               />
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="discord">Qual o seu discord?</label>
-              <Input type="text" id="discord" placeholder="Usuario#0000" />
+              <Input
+                type="text"
+                id="discord"
+                name="discord"
+                placeholder="Usuario#0000"
+                required
+              />
             </div>
           </div>
 
@@ -152,14 +195,37 @@ export function CreateAdModal() {
             <div className="flex flex-col gap-2 flex-1">
               <label htmlFor="hourStart">Qual o horário do dia?</label>
               <div className="grid grid-cols-2 gap-2">
-                <Input type="time" id="hourStart" placeholder="De" />
-                <Input type="time" id="hourEnd" placeholder="Até" />
+                <Input
+                  type="time"
+                  id="hourStart"
+                  name="hourStart"
+                  placeholder="De"
+                  required
+                />
+                <Input
+                  type="time"
+                  id="hourEnd"
+                  name="hourEnd"
+                  placeholder="Até"
+                  required
+                />
               </div>
             </div>
           </div>
 
           <label className="mt-2 flex gap-2 text-sm items-center cursor-pointer">
-            <Checkbox.Root className="w-6 h-6 p-1 rounded bg-zinc-900">
+            <Checkbox.Root
+              checked={useVoiceChannel}
+              className="w-6 h-6 p-1 rounded bg-zinc-900"
+              required
+              onCheckedChange={(checked) => {
+                if (checked === true) {
+                  setUseVoiceChannel(true);
+                } else {
+                  setUseVoiceChannel(false);
+                }
+              }}
+            >
               <Checkbox.Indicator>
                 <Check className="w-4 h-4 text-emerald-500" />
               </Checkbox.Indicator>
@@ -168,12 +234,16 @@ export function CreateAdModal() {
           </label>
 
           <footer className="mt-4 flex justify-end gap-4">
-            <Dialog.Close className="bg-zinc-500 px-5 h-12 rounded-md font-semibold hover:bg-zinc-600">
+            <Dialog.Close
+              className="bg-zinc-500 px-5 h-12 
+              rounded-md font-semibold hover:bg-zinc-600"
+            >
               Cancelar
             </Dialog.Close>
             <button
               type="submit"
-              className="bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600"
+              className="bg-violet-500 px-5 h-12 rounded-md 
+              font-semibold flex items-center gap-3 hover:bg-violet-600"
             >
               <GameController className="w-6 h-6" />
               Encontrar duo
