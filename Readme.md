f971406c768fef15f87b084456abdf64260caac5
marcelosantana <marcelosantana@alu.ufc.br>
2023-01-15 00:16:20 -0300

    [NLW-eSports] - Add Prints

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Telas - Aplicação Web 🔥
+
+Home           |  Modal de Adicionar Anúncio           
+:-------------------------:|:-------------------------:
+<img src= "./prints/Web/Home.jpg">  |  <img src= "./prints/Web/Modal.jpg">  
+
+# Telas - Aplicação Mobile 🔥
+
+Home            |  Anúncios          | Modal           
+:-------------------------:|:-------------------------:|:-------------------------:
+<img src= "./prints/Mobile/HomeMob.PNG">  |  <img src= "./prints/Mobile/Games.PNG">  | <img src="./prints/Web/Modal.jpg">
--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -10,7 +10,7 @@ export default function GameBanner({
   adsCount,
 }: GameBannerPros) {
   return (
-    <a href="" className="relative rounded-lg overflow-hidden">
+    <div className="relative rounded-lg overflow-hidden">
       <img src={bannerUrl} alt="" />
       <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
         <strong className="font-bold text-white block">{title}</strong>
@@ -18,6 +18,6 @@ export default function GameBanner({
           {adsCount} anúncio(s)
         </span>
       </div>
-    </a>
+    </div>
   );
 }
